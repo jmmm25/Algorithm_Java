fa7c83a3fb2cfbc54a7199979cbab72278e5a33d
김정민 <jmkim9302@gmail.com>
2021-11-21 17:04:09 +0900

    Add BOJ 직사각형에서_탈출_1085 solution

---
+++ src/test/java/boj/class2/직사각형에서_탈출_1085/Main.java
@@ -0,0 +1,21 @@
+package boj.class2.직사각형에서_탈출_1085;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final String PATH = "src/test/java/boj/class2/직사각형에서_탈출_1085/input.txt";
+        System.setIn(new FileInputStream(PATH));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+        int y = Integer.parseInt(stringTokenizer.nextToken());
+        int w = Integer.parseInt(stringTokenizer.nextToken());
+        int h = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(Math.min(Math.min(x, y), Math.min(h-y, w-x)));
+    }
+}
