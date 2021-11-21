9c173425b3446d8ab8c26cfb37286d15552a3071
김정민 <jmkim9302@gmail.com>
2021-11-22 08:17:19 +0900

    Add BOJ 최대공약수와_최소공배수_2609 solution

---
+++ src/test/java/boj/class2/최대공약수와_최소공배수_2609/Main.java
@@ -0,0 +1,33 @@
+package boj.class2.최대공약수와_최소공배수_2609;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final String PATH = "src/test/java/boj/class2/최대공약수와_최소공배수_2609/input.txt";
+        System.setIn(new FileInputStream(PATH));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(gcd(a, b));
+        System.out.println(lcd(a, b));
+    }
+
+    private static int lcd(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
